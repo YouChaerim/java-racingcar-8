e78eb273120ae4932d715b6aa4730aac86e0cb37
YouChaerim <ycr3080@naver.com>
2025-10-27 11:10:50 +0900

    fead: 시도 횟수검증 기능 구현

---
+++ src/main/java/racingcar/model/GameRound.java
@@ -0,0 +1,35 @@
+package racingcar.model;
+
+import static racingcar.constants.Symbol.ZERO;
+
+public class GameRound {
+    private final Integer round;
+
+    public GameRound(String round) {
+        this.round = validateRound(round);
+    }
+
+    private Integer validateRound(String round) {
+        try {
+            Integer parsedRound = Integer.parseInt(round);
+            validatePositive(parsedRound);
+            return parsedRound;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수는 숫자만 입력해야 합니다.");
+        }
+    }
+
+    private void validatePositive(Integer round) {
+        if (isNotPositive(round)) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상의 값이어야 합니다.");
+        }
+    }
+
+    private boolean isNotPositive(Integer round) {
+        return round < ZERO;
+    }
+
+    public Integer getRound() {
+        return round;
+    }
+}
