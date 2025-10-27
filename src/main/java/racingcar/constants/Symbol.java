83869827987d240d7052527298fa5a61e7cc8021
YouChaerim <ycr3080@naver.com>
2025-10-27 10:48:20 +0900

    feat: 자동차 도메인 모델 및 유효성 검증 구현

---
+++ src/main/java/racingcar/constants/Symbol.java
@@ -0,0 +1,8 @@
+package racingcar.constants;
+
+public class Symbol {
+    public static final String COLON = ":";
+    public static final String SPACE = " ";
+    public static final String HYPHEN = "-";
+    public static final Integer ZERO = 0;
+}
---
+++ src/main/java/racingcar/model/CarName.java
@@ -0,0 +1,52 @@
+package racingcar.model;
+
+public class CarName {
+    private static final Integer maxNameLength = 5;
+    private static final String letterRegex = "[a-zA-Z]+";
+    final String name;
+
+    public CarName(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        validateCarNameLength(name);
+        validateEmpty(name);
+        validateOnlyLetters(name);
+    }
+
+    private void validateCarNameLength(String name) {
+        if (IsNotValidNameLength(name)) {
+            throw new IllegalArgumentException("이름은 5글자 이하만 가능합니다.");
+        }
+    }
+
+    private boolean IsNotValidNameLength(String name) {
+        return name.length() > maxNameLength;
+    }
+
+    private void validateEmpty(String name) {
+        if (isEmptyName(name)) {
+            throw new IllegalArgumentException("공백은 입력할 수 없습니다.");
+        }
+    }
+
+    private boolean isEmptyName(String name) {
+        return name.isBlank();
+    }
+
+    private void validateOnlyLetters(String name) {
+        if (isValidOnlyLetters(name)) {
+            throw new IllegalArgumentException("이름은 영문자만 포함해야 합니다.");
+        }
+    }
+
+    private boolean isValidOnlyLetters(String name) {
+        return !name.matches(letterRegex);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
